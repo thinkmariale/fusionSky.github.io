eebbfab1c3cdbbb644ba126b728f714e35346e1d
Mariale Montenegro <thinkmariale@gmail.com>
2020-04-02 17:34:46 -0700

    Updates

---
+++ precache-manifest.a5a0bba8d24dce0e6de97848a372e23e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "471cb1aed73199a70afa368308b3d043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ad2cbfb74a777337a26",
+    "url": "/static/css/main.2a81f26b.chunk.css"
+  },
+  {
+    "revision": "34708d3f16f3ff5b9ee2",
+    "url": "/static/js/2.8326f9be.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8326f9be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad2cbfb74a777337a26",
+    "url": "/static/js/main.bae8c865.chunk.js"
+  },
+  {
+    "revision": "96a3c9d25cc575ce73ab",
+    "url": "/static/js/runtime-main.7ccdfbc5.js"
+  }
+]);
